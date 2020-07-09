3d56fa0c907ab1b56263b81c27fcec79c32705e1
ZT <1450026690@qq.com>
2020-07-09 22:35:26 +0800

    完成图书购物车小案例[C

--- Vue.js/Vue-demo/html_demo/08-图书购物车案例/main.js
+++ Vue.js/Vue-demo/html_demo/08-图书购物车案例/main.js
@@ -1,14 +1,56 @@
 const app = new Vue({
-    el: '#app',
-    data: {
-        books:[
-            {
-                id:1,
-                name:'书籍1',
-                count:3,
-                
-            }
-        ]
+  el: '#app',
+  data: {
+    books: [
+      {
+        id: 1,
+        name: '数学之美',
+        price: 100,
+        count: 1,
+      },
+      {
+        id: 2,
+        name: 'python核心编程',
+        price: 99,
+        count: 1,
+      },
+      {
+        id: 3,
+        name: '故事会',
+        price: 198,
+        count: 1,
+      },
+      {
+        id: 4,
+        name: '编程珠玑',
+        price: 66,
+        count: 1,
+      },
+    ],
+  },
+  computed: {
+    totalPrice() {
+      let resPrice = 0
+      for (let i of this.books) {
+        resPrice += i.price
+      }
+      return resPrice
     },
-    methods: {}
-});
+  },
+  methods: {
+    sub(index) {
+      this.books[index].count -= 1
+    },
+    add(index) {
+      this.books[index].count += 1
+    },
+    removeBook(index) {
+      this.books.splice(index, 1)
+    },
+  },
+  filters: {
+    showPrice(price) {
+      return '￥' + price
+    },
+  },
+})
