4ebf3eaa06882bcce5be178547ed98ab4f056292
stutao <1450026690@qq.com>
2020-07-09 17:23:55 +0800

    购物车案例-未完成

---
+++ Vue.js/Vue-demo/html_demo/08-图书购物车案例/main.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        books:[
+            {
+                id:1,
+                name:'书籍1',
+                count:3,
+                
+            }
+        ]
+    },
+    methods: {}
+});
